90fedd0fdb6317a74a05d173ca6def9c7d22b631
k4kumar <kumarjitdutta581@gmail.com>
2018-05-09 00:26:35 +0600

    Expense View update, Earnings Model & Controller Added

---
+++ MonthlyExpense/Controllers/EarningsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonthlyExpense.Controllers
+{
+    public class EarningsController : Controller
+    {
+        // GET: Earnings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MonthlyExpense/Models/EarningsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonthlyExpense.Models
+{
+    public class EarningsModel
+    {
+        public int ID { get; set; }
+        public string Source { get; set; }
+        public int Amount { get; set; }
+        public string DtEarn { get; set; }
+        public int OutstandingAmount { get; set; }
+        public string Comment { get; set; }
+    }
+}
