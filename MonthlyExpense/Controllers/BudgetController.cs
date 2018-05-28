3fdd7db9c280253c86948f2076d40f3ee07dad82
k4kumar <kumarjitdutta581@gmail.com>
2018-05-28 13:22:34 +0600

    Budget Model & Controller Added

---
+++ MonthlyExpense/Controllers/BudgetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonthlyExpense.Controllers
+{
+    public class BudgetController : Controller
+    {
+        // GET: Budget
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MonthlyExpense/Models/BudgetModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonthlyExpense.Models
+{
+    public class BudgetModel
+    {
+        public int Id { get; set; }
+        public string ExpenseType { get; set; }
+        public int Amount { get; set; }
+        public string Comment { get; set; }
+
+        public string BudgetMonth { get; set; }
+    }
+}
